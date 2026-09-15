236ea2f0fbdfd528b451d6b6ec9ad2096667565f
Do2Earn <79559773+do2earn@users.noreply.github.com>
2026-09-15 19:01:01 +0330

    Connect Earn $STEEM button to earnsteem page

--- js/home-buttons-patch.js
+++ js/home-buttons-patch.js
@@ -2,4 +2,6 @@ window.addEventListener('steemflags:ready',()=>{
   const style=document.createElement('style');
   style.textContent='.homeTitle~#newGameButton,.earnSteemButton{display:block!important;width:40%!important;min-width:0!important;margin-left:auto!important;margin-right:auto!important}.gameStartInstructions{text-align:center!important}.gameStartInstructions>div{text-align:center!important}';
   document.head.appendChild(style);
+  const earnSteemButton=document.getElementById('earnSteemButton');
+  if(earnSteemButton)earnSteemButton.href='./earnsteem.html';
 },{once:true});
